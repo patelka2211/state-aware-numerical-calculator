08c4ca46da4f88f446c1db5b3b4af801890a7e68
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2023-03-12 17:56:34 +0530

    Remaking whole project.

--- renderer.js
+++
@@ -1,7 +0,0 @@
-let electron = require("electron");
-
-let { ipcRenderer } = electron;
-
-document.getElementById("minimize-btn").addEventListener("click", () => {
-    ipcRenderer.send("window:navigation", "minimize");
-});
