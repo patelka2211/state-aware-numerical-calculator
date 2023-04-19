40d70e9ba053b0d00c7e8f99276295c5db88b16a
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2023-04-19 16:59:10 +0530

    Sharer shortcut added

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ elements.btn_equal().addEventListener("click", (e) => {
 });
 const keys_array = "0123456789/*+-=.".split("");
 function keyMappings(showAlert = false) {
-    let msg_str = `Here are the key mappings you can use:\n\nKey 'c' or 'C' : Clear button\nKey '0' : 0 button\nKey '1' : 1 button\nKey '2' : 2 button\nKey '3' : 3 button\nKey '4' : 4 button\nKey '5' : 5 button\nKey '6' : 6 button\nKey '7' : 7 button\nKey '8' : 8 button\nKey '9' : 9 button\nKey '/' : Division button\nKey '*' or 'x' or 'X' : Multiplication button\nKey '+' : Addition button\nKey '-' : Subtraction button\nKey '.' : Period button\nKey '=' or 'Enter key' : Equal button\nKey 'k' or 'K' : To see key mappings`;
+    let msg_str = `Here are the key mappings you can use:\n\nKey 's' or 'S': To share this page\nKey 'c' or 'C' : Clear button\nKey '0' : 0 button\nKey '1' : 1 button\nKey '2' : 2 button\nKey '3' : 3 button\nKey '4' : 4 button\nKey '5' : 5 button\nKey '6' : 6 button\nKey '7' : 7 button\nKey '8' : 8 button\nKey '9' : 9 button\nKey '/' : Division button\nKey '*' or 'x' or 'X' : Multiplication button\nKey '+' : Addition button\nKey '-' : Subtraction button\nKey '.' : Period button\nKey '=' or 'Enter key' : Equal button\nKey 'k' or 'K' : To see key mappings\n`;
     if (showAlert)
         alert(msg_str);
     else
@@ -55,6 +55,9 @@ document.addEventListener("keypress", (e) => {
         nextCharacter("=");
     else if ("kK".indexOf(e.key) !== -1)
         keyMappings(true);
+    else if ("sS".indexOf(e.key) !== -1) {
+        new Function(`try{sharer.open();}catch(error){console.log(error);}`)();
+    }
 });
 function runIfOnDesktop() {
     setTimeout(() => {
